bccb4512bdd96273a0f2a6b63d936584698263cd
Vladyslav Lykhovyd <v.lykhovyd@urancompany.com>
2022-09-15 20:48:26 +0300

    Initial Commit

---
+++ githubSquare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubSquare
+//
+//  Created by admin on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
